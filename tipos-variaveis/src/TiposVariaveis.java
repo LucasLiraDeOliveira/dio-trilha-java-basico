cf46424ab4b9a2d04f97ba0e962aa36386ad7be8
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-06-19 16:18:13 -0300

    adicionado arquivo referente a aula de tipos primitivos e String

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // ver melhor sobre tipos primitivos
+        // ver melhor sobre classe String que representa texto
+
+        String meuNome = "Lucas Lira";
+        double salarioMinimo = 2500;
+
+        // casting é quando se quer converter um valor de uma variavel de um tipo maior para um tipor menor. pois para o contrario o java dfaz de boa. ex.:
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //variaveis e constates:
+        int numero = 1;
+
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
