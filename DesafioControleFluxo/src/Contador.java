140ff7f8a8e6acf72b7ff16993451eb2eb50d468
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-06-29 23:14:59 -0300

    finished the project main file in which I practiced Control Flow in Java

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,3 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Contador {
-    
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+        //next
+        //nextDouble
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+            terminal.close();
+		
+		}catch (ParametrosInvalidosException e) {
+            System.out.println("Por favor reinicie o código! O segundo número deve ser maior que o primeiro!");
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if(parametroUm > parametroDois)
+            throw new ParametrosInvalidosException();
+		
+		int contagem = parametroDois - parametroUm;
+        System.out.println("A diferença entre o segundo número e o primeiro número é de " + contagem);
+        for(int i=1; i <= contagem; i++){
+            System.out.println("Imprimindo número " + i);
+        }
+	}
 }
+
