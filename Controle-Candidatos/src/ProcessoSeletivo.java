93a165e626ede7d85a172c514f9b1338417942a6
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-06-29 22:21:06 -0300

    Add exemples to Controle-Candidatos project

--- Controle-Candidatos/src/ProcessoSeletivo.java
+++ Controle-Candidatos/src/ProcessoSeletivo.java
@@ -1,5 +1,26 @@
+// case 1: Início do processo seletivo onde avaliará a proposta salarial do do possível funcionário e o teto que a empresa está disposta a pagar
+
 public class ProcessoSeletivo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) {
+		//salario base maior que salario pretendido
+		case1(2000.0, 1900.0);
+		
+		//salario base igual que salario pretendido
+		case1(2000.0, 2000.0);
+		
+		//salario base igual que salario pretendido
+		case1(1900.0, 2000.0);
+	}
+	static void case1(double salarioBase, double salarioPretendido) {
+		
+		if(salarioBase > salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO");
+		}
+		else if(salarioBase == salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+		}
+		else {
+			System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+		}
+	}
 }
