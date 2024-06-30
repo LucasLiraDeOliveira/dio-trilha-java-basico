1d3092170764416719322eea63c80b59074494e8
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-06-29 23:59:13 -0300

    Cleaning unnecessary comments

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -7,23 +7,21 @@ public class Contador {
 
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
-        //next
-        //nextDouble
 
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
 		
 		try {
-			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
             terminal.close();
 		
 		}catch (ParametrosInvalidosException e) {
             System.out.println("Por favor reinicie o código! O segundo número deve ser maior que o primeiro!");
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 		}
 		
 	}
+
+    
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         if(parametroUm > parametroDois)
             throw new ParametrosInvalidosException();
