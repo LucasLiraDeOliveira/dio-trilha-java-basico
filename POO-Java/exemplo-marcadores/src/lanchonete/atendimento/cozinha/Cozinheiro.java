ccd2328ce3f2c2a3794d423ae7b102e7313e447b
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-07-02 21:11:11 -0300

    Added project to study about Java Modifiers

---
+++ POO-Java/exemplo-marcadores/src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -0,0 +1,58 @@
+package lanchonete.atendimento.cozinha;
+
+public class Cozinheiro {
+	//pode ser default
+	public void adicionarLancheNoBalcao() {
+		prepararCombo();
+		System.out.println("ADICIONANDO LANCHE NATURAL HAMBÚRGUER NO BALCÃO");
+	}
+	//pode ser default
+	public void adicionarSucoNoBalcao() {
+		System.out.println("ADICIONANDO SUCO NO BALCÃO");
+	}
+	//pode ser default
+	public void adicionarComboNoBalcao() {
+		adicionarLancheNoBalcao();
+		adicionarSucoNoBalcao();
+	}
+	private void prepararLanche() {
+		pedirIngredientes(null);
+		lavarIngredientes();
+		selecionarIngredientesLanche();
+		System.out.println("PREPARANDO LANCHE TIPO HAMBÚRGUER");
+	}
+	private void prepararVitamina() {
+		selecionarIngredientesVitamina();
+		baterVitaminaLiquidificador();
+		fritarIngredientesLanche();
+		System.out.println("PREPARANDO SUCO");
+	}
+	private void prepararCombo() {
+		prepararLanche();
+		prepararVitamina();
+	}
+	private void selecionarIngredientesLanche() {
+		System.out.println("SELECIONADO O PÃO, SALADA, OVO E CARNE");
+	}
+	private void selecionarIngredientesVitamina() {
+		System.out.println("SELECIONADO FRUTA, LEITE E SUCO");
+	}
+	private void lavarIngredientes() {
+		System.out.println("LAVANDO INGREDIENTES");
+	}
+	private void baterVitaminaLiquidificador() {
+		System.out.println("BATENDO VITAMINA NO LIQUIDIFICADOR");
+	}
+	private void fritarIngredientesLanche() {
+		System.out.println("FRITANDO A CARNE E OVO PARA O HAMBÚRGUER");
+	}
+	@SuppressWarnings("unused")
+	private void pedirParaTrocarGas(Almoxarife meuAmigo) {
+		meuAmigo.trocarGas();
+	}
+	private void pedirIngredientes(Almoxarife almoxarife) {
+		almoxarife.entregarIngredientes();
+	}
+}
+
+
